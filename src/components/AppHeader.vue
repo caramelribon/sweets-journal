ed84311827c89115f47e6859851554904eb3cd7a
wakana.tatsuta <caramelribon137@gmail.com>
2021-12-12 13:45:00 +0000

    navigation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,14 +2,20 @@
   <!-- navigation -->
   <div class="navber">
     <!-- ログイン中 -->
-    <nav id="header" class="w-full z-30 top-0 py-1">
+    <nav id="header" class="w-full z-30 top-0 py-1" v-if="currentUID !== null">
       <div
       class="w-full container mx-auto flex flex-wrap items-center
       justify-between mt-0 px-6 py-3">
-        <div class="order-1 md:order-2">
-          <router-link class="font-semibold text-xl tracking-tight" v-bind:to=item.path>
-            {{ item.title }}
-          </router-link>
+        <div class="text-sm lg:flex-grow"
+        v-for="(item, index) in items" :key="index">
+          <div class="flex">
+            <router-link
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200
+            hover:text-white"
+            v-bind:to=item.path>
+              {{ item.title }}
+            </router-link>
+          </div>
         </div>
         <div class="order-2 md:order-3 flex items-center balloonoya" id="nav-content">
           <button
@@ -28,16 +34,51 @@
               2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z"></path>
             </svg>
           </button>
-          <span class="balloon1" id="makeImg">
-            <div>
-              
+          <span id="userinfo">
+            <div class="userinfo" v-if="userinfo">
+              <div class="border-b">
+                <i class="fas fa-user-circle fa-5x text-center"></i>
+                <p class="text-center m-2">{{ username }}</p>
+              </div>
+              <div class="button">
+                <div class="flex justify-center items-center mx-auto">
+                  <!-- Sginup -->
+                  <div class="signup px-1">
+                    <button
+                      class="inline-block text-sm px-4 py-2 leading-none border rounded text-white
+                      border-white hover:border-transparent hover:text-teal-500 hover:bg-white
+                      mt-4 lg:mt-0"
+                      type="button"
+                      @click="openSignupForm">
+                        Sginup
+                    </button>
+                    <signup-modal @close="closeSignupForm" v-if="signupmodal"></signup-modal>
+                  </div>
+                  <!-- Login -->
+                  <div class="login">
+                    <button
+                      class="inline-block text-sm px-4 py-2 leading-none border rounded text-white
+                      border-white hover:border-transparent hover:text-teal-500 hover:bg-white
+                      mt-4 lg:mt-0"
+                      type="button"
+                      @click="openLoginForm">
+                        Login
+                    </button>
+                    <login-modal @close="closeLoginForm" v-if="loginmodal"></login-modal>
+                  </div>
+                </div>
+              </div>
+              <div class="message">
+                <p class="m-2">会員登録(無料)すれば、気に入ったお店を登録できたり、
+                気になるお店をマークすることができます^^ また、全ユーザのアクティビティも見れますよ♪</p>
+              </div>
             </div>
           </span>
         </div>
       </div>
     </nav>
     <!-- 未ログイン -->
-    <nav id="header" class="w-full z-30 top-0 py-1">
+    <nav class="w-full z-30 top-0 py-1" v-else>
       <div
       class="w-full container mx-auto flex flex-wrap items-center
       justify-between mt-0 px-6 py-3">
@@ -63,85 +104,46 @@
               2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z"></path>
             </svg>
           </button>
-          <span class="balloon1" id="makeImg">
-            <div v-if="userinfo">
-              
+          <span id="userinfo">
+            <div class="userinfo" v-if="userinfo">
+              <div class="border-b">
+                <i class="fas fa-user-circle fa-5x text-center"></i>
+                <p class="text-center m-2">ゲスト</p>
+              </div>
+              <div class="button">
+                <div class="flex justify-center items-center mx-auto">
+                  <!-- Sginup -->
+                  <div class="signup px-1">
+                    <button
+                      class="inline-block text-sm px-4 py-2 leading-none border rounded text-white
+                      border-white hover:border-transparent hover:text-teal-500 hover:bg-white
+                      mt-4 lg:mt-0"
+                      type="button"
+                      @click="openSignupForm">
+                        Sginup
+                    </button>
+                    <signup-modal @close="closeSignupForm" v-if="signupmodal"></signup-modal>
+                  </div>
+                  <!-- Login -->
+                  <div class="login">
+                    <button
+                      class="inline-block text-sm px-4 py-2 leading-none border rounded text-white
+                      border-white hover:border-transparent hover:text-teal-500 hover:bg-white
+                      mt-4 lg:mt-0"
+                      type="button"
+                      @click="openLoginForm">
+                        Login
+                    </button>
+                    <login-modal @close="closeLoginForm" v-if="loginmodal"></login-modal>
+                  </div>
+                </div>
+              </div>
+              <div class="message">
+                <p class="m-2">会員登録(無料)すれば、気に入ったお店を登録できたり、
+                気になるお店をマークすることができます^^ また、全ユーザのアクティビティも見れますよ♪</p>
+              </div>
             </div>
           </span>
-        </div>
-      </div>
-    </nav>
-    
-    <nav class="flex items-center justify-between flex-wrap p-6">
-      <div
-      class="flex items-center flex-shrink-0 text-white mr-6"
-      v-for="(item, index) in tops" :key="index">
-        <router-link class="font-semibold text-xl tracking-tight" v-bind:to=item.path>
-          {{ item.title }}
-        </router-link>
-      </div>
-      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div v-if="currentUID !== null">
-          <div class="text-sm lg:flex-grow"
-          v-for="(item, index) in items" :key="index">
-            <div class="flex">
-              <router-link
-              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200
-              hover:text-white"
-              v-bind:to=item.path>
-                {{ item.title }}
-              </router-link>
-            </div>
-          </div>
-        </div>
-        <div v-if="currentUID !== null" class="flex">
-          <!-- Login UserName -->
-          <div class="login-username px-1">
-            <button
-            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white
-            border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-            type="button"
-            >
-              {{ username }}
-            </button>
-          </div>
-          <div class="logout">
-            <button
-              class="inline-block text-sm px-4 py-2 leading-none border rounded text-white
-              border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-              type="button"
-              @click="onClickLogOut">
-                Logout
-            </button>
-          </div>
-        </div>
-        <div v-else>
-          <div class="flex">
-            <!-- Sginup -->
-            <div class="signup px-1">
-              <button
-                class="inline-block text-sm px-4 py-2 leading-none border rounded text-white
-                border-white hover:border-transparent hover:text-teal-500 hover:bg-white
-                mt-4 lg:mt-0"
-                type="button"
-                @click="openSignupForm">
-                  Sginup
-              </button>
-              <signup-modal @close="closeSignupForm" v-if="signupmodal"></signup-modal>
-            </div>
-            <!-- Login -->
-            <div class="login">
-              <button
-                class="inline-block text-sm px-4 py-2 leading-none border rounded text-white
-                border-white hover:border-transparent hover:text-teal-500 hover:bg-white
-                mt-4 lg:mt-0"
-                type="button"
-                @click="openLoginForm">
-                  Login
-              </button>
-              <login-modal @close="closeLoginForm" v-if="loginmodal"></login-modal>
-            </div>
-          </div>
         </div>
       </div>
     </nav>
@@ -158,10 +160,8 @@ export default {
   data() {
     return {
       tops: [{ title: 'Sweets Journal', path: '/' }],
-      items: [
-        { title: 'Profile', path: '/profile' },
-        { title: 'Activity', path: '/activity' },
-      ],
+      items: [{ title: 'Profile', path: '/profile' }],
+      activities: [{ title: 'Activity', path: '/activity' }],
       loginmodal: false,
       signupmodal: false,
       currentUID: null,
@@ -235,4 +235,23 @@ export default {
 </script>
 
 <style>
+.userinfo {
+  position: absolute;
+  display: block;
+  padding: 2px;
+  background-color: rgba(102, 102, 255, 0.50);
+  width:200px;
+  height: 350px;
+  right: 150%;
+  top: 0.5%;
+}
+.userinfo:after {
+  border-left: 12px solid rgba(102, 102, 255, 0.50);
+  border-top: 10px solid transparent;
+  border-bottom: 10px solid transparent;
+  right: -12px;
+  top: 1.5%;
+  content: '';
+  position: absolute;
+}
 </style>
