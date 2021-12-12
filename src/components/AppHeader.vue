2742a96d1c849b9fcc690af364d2c1daddfd23b0
wakana.tatsuta <caramelribon137@gmail.com>
2021-12-12 13:07:20 +0000

    userinfo show

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,77 @@
 <template>
   <!-- navigation -->
   <div class="navber">
+    <!-- ログイン中 -->
+    <nav id="header" class="w-full z-30 top-0 py-1">
+      <div
+      class="w-full container mx-auto flex flex-wrap items-center
+      justify-between mt-0 px-6 py-3">
+        <div class="order-1 md:order-2">
+          <router-link class="font-semibold text-xl tracking-tight" v-bind:to=item.path>
+            {{ item.title }}
+          </router-link>
+        </div>
+        <div class="order-2 md:order-3 flex items-center balloonoya" id="nav-content">
+          <button
+          class="inline-block no-underline hover:text-black"
+          @click="showBalloon">
+            <svg
+            class="fill-current hover:text-black"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24">
+              <circle fill="none" cx="12" cy="7" r="3"></circle>
+              <path d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12
+              10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21
+              21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757
+              2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z"></path>
+            </svg>
+          </button>
+          <span class="balloon1" id="makeImg">
+            <div>
+              
+            </div>
+          </span>
+        </div>
+      </div>
+    </nav>
+    <!-- 未ログイン -->
+    <nav id="header" class="w-full z-30 top-0 py-1">
+      <div
+      class="w-full container mx-auto flex flex-wrap items-center
+      justify-between mt-0 px-6 py-3">
+        <div class="order-1 md:order-2">
+          <router-link class="font-semibold text-xl tracking-tight" v-bind:to=item.path>
+            {{ item.title }}
+          </router-link>
+        </div>
+        <div class="order-2 md:order-3 flex items-center balloonoya" id="nav-content">
+          <button
+          class="inline-block no-underline hover:text-black"
+          @click="showBalloon">
+            <svg
+            class="fill-current hover:text-black"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24">
+              <circle fill="none" cx="12" cy="7" r="3"></circle>
+              <path d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12
+              10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21
+              21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757
+              2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z"></path>
+            </svg>
+          </button>
+          <span class="balloon1" id="makeImg">
+            <div v-if="userinfo">
+              
+            </div>
+          </span>
+        </div>
+      </div>
+    </nav>
+    
     <nav class="flex items-center justify-between flex-wrap p-6">
       <div
       class="flex items-center flex-shrink-0 text-white mr-6"
@@ -95,6 +166,7 @@ export default {
       signupmodal: false,
       currentUID: null,
       username: null,
+      userinfo: false,
     };
   },
   async created() {
@@ -127,12 +199,14 @@ export default {
     },
     closeLoginForm() {
       this.loginmodal = false;
+      this.userinfo = false;
     },
     openSignupForm() {
       this.signupmodal = true;
     },
     closeSignupForm() {
       this.signupmodal = false;
+      this.userinfo = false;
     },
     onClickLogOut() {
       firebase
@@ -148,6 +222,13 @@ export default {
           // ログアウトに失敗したときの処理
           console.error('ログアウトエラー', error);
         });
+    },
+    showBalloon() {
+      if(this.userinfo === false) {
+        this.userinfo = true;
+      } else {
+        this.userinfo = false;
+      }
     },
   },
 };
